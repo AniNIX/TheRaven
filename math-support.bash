524e3916d362e946044468e79a365aa81b9004af
DarkFeather <darkfeather@aninix.net>
2017-01-13 09:47:34 -0600

    Dumb grep

--- math-support.bash
+++ math-support.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-lynx -connect_timeout=3 -read_timeout=3 --dump 'http://m.wolframalpha.com/input/?i='"$(echo $1 | sed 's/+/%2B/g' | tr ' ' '+')"'&x=0&y=0' | cat -n | grep 21 | head -n 1 | xargs | cut -f 2 -d ' '
+lynx -connect_timeout=3 -read_timeout=3 --dump 'http://m.wolframalpha.com/input/?i='"$(echo $1 | sed 's/+/%2B/g' | tr ' ' '+')"'&x=0&y=0' | cat -n | egrep '^    21' | head -n 1 | xargs | cut -f 2 -d ' '

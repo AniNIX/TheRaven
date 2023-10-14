f0685bc0af653d7355b8f2aadaa1e930dd35a76a
DarkFeather <ircs://aninix.net:6697/DarkFeather>
2023-10-14 13:12:53 -0500

    Updating for hooks

--- math-support.bash
+++ math-support.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-lynx -connect_timeout=3 -read_timeout=3 --dump 'http://m.wolframalpha.com/input/?i='"$(echo $1 | sed 's/+/%2B/g' | tr ' ' '+')"'&x=0&y=0' | cat -n | egrep '^    21' | head -n 1 | xargs | sed 's/^21 //'
+lynx -connect_timeout=3 -read_timeout=3 --dump 'http://m.wolframalpha.com/input/?i='"$(echo $1 | sed 's/+/%2B/g' | tr ' ' '+')"'&x=0&y=0' | cat -n | grep -E '^    21' | head -n 1 | xargs | sed 's/^21 //'
